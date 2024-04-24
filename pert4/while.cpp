2b6b0c076bf13e74c3d0a98537a6eba88d039846
raihanfadhliramadhannnn <raihanfadhliir@gmail.com>
2024-04-24 14:22:35 +0700

    test

---
+++ pert4/while.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+    //maksude piyee?
+
+    void penggunaandowhile(){
+        do
+        {
+            /* code */
+        } while (/* condition */);
+        
+
+
+    }
+int main(){
+    cout << "Iteration statement" << endl;
+    cout << "Penggunaan FOR" << endl;
+
+    // int a = 1;
+    // apa bedanya int di luar for dengan int didalam for ??
+
+    for (int a = 1; a <=5; a++){
+        cout << a << endl;
+    }
+    return 0;
+}
